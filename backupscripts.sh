3e2066d687eb38762e0a94eb3298009d01e9f3a6
skumar0710 <36155226+skumar0710@users.noreply.github.com>
2018-05-29 09:29:15 +0530

    Update backupscripts.sh

--- backupscripts.sh
+++ backupscripts.sh
@@ -1,61 +1,50 @@
-#! /bin/bash
-#This script is used to create a backup of folders to target location with user input
+#!/bin/bash
+#This script is used to create a backup of filesystem or folders to target location based on user inputs.
 
-echo "Enter the full path name of the folder you want to backup:\c"dd
 clear
-backupfiles=backup.`date +%F`.`date +%k.%M`.tgz
-echo "Enter the full path name of the folder you want to backup: \c"
+
+#Files will be stored in the below format.
+time=`date +%k.%M.%S | cut -f2 -d " "`
+dateformat=`date +%F`
+backupfiles=backup.$dateformat.$time.tgz
+
+#Getting the user input of a folder or filesystem require backup.
+
+echo -e "Enter the full path of the folder or filesystem you want to backup: \c"
 read backupfolders
-if [ ! -e $backupfolders ]
+if [ ! -e $backupfolders ];
 then
-        echo "\n\nYou have typed incorrect path, So exiting from the backup script........\n";
-        sleep 2;
-        exit;
+        echo -e "\n\nYou have typed incorrect path, So exiting from the backup script........\n"
+        exit
 fi
 
-echo "\nWhere do you want to keep the backup files.Enter the Full path: \c"
-read backupdir
+#Getting user input to store the files
+echo -e "Enter the target path to store the backup files: \c"
+read targetdir
 
-if [ ! -e $backupdir ]
+if [ ! -e $targetdir ];
 then
-        echo "Backup Folder doesn't exist. Creating the Folder......";
-        sleep 2;
-        mkdir -p $backupdir;
-        if [ $? = 0 ]
+        echo -e "Backup Folder doesn't exist. Creating the Folder......"
+        mkdir -p $targetdir
+
+fi
+
+#calculating the total size of the source folder and also checking free space available at the target location.
+totalsize=`du -sk $backupfolders | awk '{print $1}'`
+freespace=`df $targetdir | grep -v Filesystem | awk '{print $4}'`
+
+#Checking free space available or not to store the data.
+if [ $totalsize -le $freespace ];
+then
+        echo -e "Target location has enough free space, hence proceeding to store the data...\n"
+        echo -e "Backup process started to take the backup $backupfolders. Please wait for a moment..."
+        tar -czvf $targetdir/$backupfiles $backupfolders > /dev/null
+        if [ $? -eq 0 ];
         then
-                echo "Backup Folder $backupdir created successfully";
-                sleep 2;
+           echo -e "\nBackup process is completed. Backup File is saved in $targetdir.\n"
         else
-                echo "\nError has occured, while creating the Backup folder.\n";
+           echo -e "ERROR : Backup is failed\n"
         fi
-        sleep 2;
 else
-        limit=7
-        count=`ls $backupdir/backup* | wc -l`
-        echo "Counting your existing backup files in the $backupdir..";
-        sleep 2;
-        if [ $count -le $limit ]
-        then
-                echo "Total Backup Files\\t\t\t\t\t\t\t : $count";
-                sleep 2;
-        else
-                echo "Total Backup Files\\t\t\t\t\t\t\t : $count";
-                sleep 2;
-        fi
-fi
-if [ $? = 0 ]
-then
-if [ $count -le $limit ]
-        then
-        clear;
-        echo -e "Backup process started to take the backup \033[33m$backupfolders.\033[0m Please wait for a moment...";
-        sleep 3;
-        tar -czvf $backupdir/$backupfiles $backupfolders;
-        echo -e "\n\n\033[32mBackup process is completed. Backup File is saved in \033[0m\033[33m$backupdir.\033[0m\n\n";
-        else
-        echo -e "\n\n\033[31mWhile creating Backup, issue has occurs. Backup file limit is maximum 7, But it exceeded the limit. So delete any other old backup files in your backup folder..\033[0m\n\n";
-fi
-else
-        echo "Error";
-
+        echo -e "ERROR : Target Location $targetdir doesnt have sufficient free space to store the data..\n"
 fi

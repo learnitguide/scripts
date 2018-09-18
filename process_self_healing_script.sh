c71714073aaabd82263e6d25ad149b45c1920642
skumar0710 <36155226+skumar0710@users.noreply.github.com>
2018-09-18 18:00:59 +0530

    Update process_self_healing_script.sh

--- process_self_healing_script.sh
+++ process_self_healing_script.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-#If an application (process) is not running for any reason, this script would monitor the process and will restart the application.
+#This script is written by me (Selva) to ensure the particular application (process) is running all the time.
+#If an application is not running for any reason, this script would monitor the process and will restart the application.
 #To do so automatically, we can add this script in cron job for particular interval.
 
 #Process name created by the application start script.
 process_name="app.py"
+
+status_log="logs"
 
 #Checking the process running or not.
 command=`ps -ef | grep -i $process_name | grep -v grep`
 
 if [ $? -eq 0 ]
 then
-    echo  -e "`date` - Application $process_name is running"
+    echo  -e "`date` - Application $process_name is running" >> $status_log
 else
-    echo -e "`date` - Application $process_name is not running, hence starting it"
 
     python /root/devops-challenge/app.py & > /dev/null
 
     if [ $? -eq 0 ]
     then
-        echo -e "Started the Application $process_name, It's running"
+        echo -e "`date` - Application $process_name is not running, hence started now" >> $status_log
     fi
 fi
